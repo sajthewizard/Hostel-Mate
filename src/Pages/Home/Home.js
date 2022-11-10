53d346325ce53d01ab8b7a2a2544c3645e888e42
sajthewizard <rsaj53@gmail.com>
2022-11-10 13:44:02 +0100

    homepage

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import useTitle from '../../hooks/useTitle';
+import HomeData from '../HomeData/HomeData';
 
 import User from '../Users/User';
 import About from './About/About';
 import Banner from './Banner/Banner';
 
 const Home = () => {
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/limservices')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    })
     useTitle('Home')
     return (
         <div>
             <Banner></Banner>
-            <About></About>
+            <About></About><div className='grid lg:grid-cols-3 md:grid-cols-1 gap-4 my-12'>
+                {
+                    services.map(service => <HomeData service={service}></HomeData>)
+                }
+            </div>
+            <div className='flex justify-end'><button className='btn text-3xl '><Link to="/services"> see more....</Link></button></div>
+
             <User></User>
 
-        </div>
+        </div >
     );
 };
 
---
+++ src/Pages/HomeData/HomeData.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link, } from 'react-router-dom';
+import 'react-photo-view/dist/react-photo-view.css';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import { FaStar } from 'react-icons/fa';
+
+
+const HomeData = ({ service }) => {
+    const { name, title, img, _id, Subscription, time, rating } = service;
+    return (
+        <div className="card w-96 bg-indigo-400 shadow-xl ">
+            <figure>
+                <PhotoProvider>
+                    <PhotoView src={img}>
+                        <img src={img} alt="" />
+                    </PhotoView>
+                </PhotoProvider>
+
+
+
+
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">
+                    {name}
+                    <div className="badge badge-secondary">{rating} <FaStar></FaStar></div>
+                </h2>
+                {title.length > 100 ? <p>{title.slice(0, 100)}  ...</p>
+                    : { title }}
+                <div className="card-actions justify-between">
+                    <Link to={`/services/${_id}`}><button className="btn btn-outline btn-accent btn-xs">Details</button></Link>
+
+                    <div className="badge badge-outline">{time}</div>
+                    <div className="badge badge-outline">{Subscription}</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HomeData;
